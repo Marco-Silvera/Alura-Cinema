aaf06a7e26971b982168d99517be3d1490b48145
Marco Silvera <2018019183@unfv.edu.pe>
2024-06-24 21:51:54 -0500

    Agragué guardado y listado de favoritos

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,16 +4,19 @@ import Favoritos from "./pages/Favoritos";
 import { default as Inicio } from "./pages/Inicio/index";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Container from "./components/Container";
+import FavoritoProvider from "./context/Favoritos";
 
 function AppRoutes() {
     return (
         <BrowserRouter>
             <Cabecera />
             <Container>
-                <Routes>
-                    <Route path="/" element={<Inicio />}></Route>
-                    <Route path="/favoritos" element={<Favoritos />} />
-                </Routes>
+                <FavoritoProvider>
+                    <Routes>
+                        <Route path="/" element={<Inicio />}></Route>
+                        <Route path="/favoritos" element={<Favoritos />} />
+                    </Routes>
+                </FavoritoProvider>
             </Container>
             <Footer />
         </BrowserRouter>
